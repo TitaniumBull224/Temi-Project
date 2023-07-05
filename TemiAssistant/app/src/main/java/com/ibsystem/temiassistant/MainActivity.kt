f590258a1343c9af11ab7b02b26814efd2fea7ed
TitaniumBull224 <titaniumyama224@gmail.com>
2023-07-05 11:17:25 +0900

    revamp temi assistant

--- TemiAssistant/app/src/main/java/com/ibsystem/temiassistant/MainActivity.kt
+++ TemiAssistant/app/src/main/java/com/ibsystem/temiassistant/MainActivity.kt
@@ -1,57 +1,38 @@
 package com.ibsystem.temiassistant
 
 import android.os.Bundle
-import android.view.Menu
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
-import com.ibsystem.temiassistant.databinding.ActivityMainBinding
+import com.robotemi.sdk.Robot
+import com.robotemi.sdk.listeners.OnRobotReadyListener
 
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
-
+class MainActivity : AppCompatActivity(), OnRobotReadyListener {
+    private lateinit var mRobot: Robot
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        // Initialize robot instance
+        mRobot = Robot.getInstance();
 
-        setSupportActionBar(binding.appBarMain.toolbar)
+    }
 
-        binding.appBarMain.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+    override fun onStart() {
+        super.onStart()
+
+        // Add robot event listeners
+        mRobot.addOnRobotReadyListener(this)
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        // Remove robot event listeners
+        mRobot.removeOnRobotReadyListener(this)
+    }
+
+    override fun onRobotReady(isReady: Boolean) {
+        if (isReady) {
+            mRobot.hideTopBar() // hide temi's top action bar when skill is active
         }
-        val drawerLayout: DrawerLayout = binding.drawerLayout
-        val navView: NavigationView = binding.navView
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
-            ), drawerLayout
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
-        return true
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
