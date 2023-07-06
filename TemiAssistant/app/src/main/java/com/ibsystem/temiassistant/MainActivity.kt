dd9ddc29eddd41bb73fe60fc857186b96097b013
VDT1805 <vdtoan1805@gmail.com>
2023-07-06 10:41:13 +0900

    hide keyboard

--- TemiAssistant/app/src/main/java/com/ibsystem/temiassistant/MainActivity.kt
+++ TemiAssistant/app/src/main/java/com/ibsystem/temiassistant/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.ibsystem.temiassistant
 
-import android.R
 import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
-import android.widget.TextView
+import android.view.MotionEvent
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.ibsystem.temiassistant.databinding.ActivityMainBinding
@@ -167,5 +168,24 @@ class MainActivity : AppCompatActivity(), OnRobotReadyListener, Robot.AsrListene
         val str = "Current Position: X: " + position.x + " Y: " + position.y
         Log.i(TAG, str)
         binding.textViewPosition.text = str
+    }
+
+    override fun dispatchTouchEvent(ev: MotionEvent): Boolean {
+        val view = currentFocus
+        if (view != null && (ev.action == MotionEvent.ACTION_UP || ev.action == MotionEvent.ACTION_MOVE) && view is EditText && !view.javaClass.name.startsWith(
+                "android.webkit."
+            )
+        ) {
+            val scrcoords = IntArray(2)
+            view.getLocationOnScreen(scrcoords)
+            val x = ev.rawX + view.getLeft() - scrcoords[0]
+            val y = ev.rawY + view.getTop() - scrcoords[1]
+            if (x < view.getLeft() || x > view.getRight() || y < view.getTop() || y > view.getBottom()) (this.getSystemService(
+                INPUT_METHOD_SERVICE
+            ) as InputMethodManager).hideSoftInputFromWindow(
+                this.window.decorView.applicationWindowToken, 0
+            )
+        }
+        return super.dispatchTouchEvent(ev)
     }
 }
