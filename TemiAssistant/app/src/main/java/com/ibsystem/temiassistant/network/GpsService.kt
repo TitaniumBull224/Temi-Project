8ec7fbd7b34c02f7c9a177fda56467f2c97d9607
VDT1805 <vdtoan1805@gmail.com>
2023-07-20 14:05:57 +0900

    Revert "Revert "get current loc""
    This reverts commit 140bdd6e1dd4fb2e0fb4ebdeb622629e96f2ddaa.

---
+++ TemiAssistant/app/src/main/java/com/ibsystem/temiassistant/network/GpsService.kt
@@ -0,0 +1,13 @@
+import android.content.Context
+import android.location.Location
+import android.location.LocationListener
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+
+class GpsService(private val context: Context) {
+
+
+
+
+}
