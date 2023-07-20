cbbcaa7045f1f25e4697840003ba100ef572f7ae
VDT1805 <vdtoan1805@gmail.com>
2023-07-20 13:57:00 +0900

    get current loc

--- TemiAssistant/app/src/main/java/com/ibsystem/temiassistant/MainActivity.kt
+++ TemiAssistant/app/src/main/java/com/ibsystem/temiassistant/MainActivity.kt
@@ -1,27 +1,36 @@
 package com.ibsystem.temiassistant
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.location.SettingInjectorService
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.CheckResult
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.core.app.ActivityCompat
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.compose.rememberNavController
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.ibsystem.temiassistant.mainscreen.Navigation
 import com.ibsystem.temiassistant.presentation.chat_ui.ChatScreenViewModel
-import com.ibsystem.temiassistant.presentation.chat_ui.Message
 import com.ibsystem.temiassistant.presentation.chat_ui.MessageBody
 import com.ibsystem.temiassistant.ui.theme.ComposeUiTempletesTheme
-import com.robotemi.sdk.NlpResult
 import com.robotemi.sdk.Robot
 import com.robotemi.sdk.listeners.OnConversationStatusChangedListener
 import com.robotemi.sdk.listeners.OnDetectionDataChangedListener
@@ -33,6 +42,7 @@ import com.robotemi.sdk.navigation.listener.OnCurrentPositionChangedListener
 import com.robotemi.sdk.navigation.model.Position
 import com.robotemi.sdk.permission.Permission
 
+
 @Suppress("DEPRECATION")
 class MainActivity : ComponentActivity(), OnRobotReadyListener, Robot.AsrListener, // Robot.NlpListener,
     OnConversationStatusChangedListener, OnCurrentPositionChangedListener,
@@ -40,11 +50,14 @@ class MainActivity : ComponentActivity(), OnRobotReadyListener, Robot.AsrListene
     private val tag = MainActivity::class.java.simpleName
     lateinit var mRobot: Robot
     lateinit var viewModel: ChatScreenViewModel
+    lateinit var fusedLocationListener: FusedLocationProviderClient
+
     @OptIn(ExperimentalFoundationApi::class, ExperimentalMaterialApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mRobot = Robot.getInstance()
         viewModel = ChatScreenViewModel(mRobot)
+        fusedLocationListener = LocationServices.getFusedLocationProviderClient(this)
         setContent {
             ComposeUiTempletesTheme() {
                 // A surface container using the 'background' color from the theme
@@ -64,7 +77,6 @@ class MainActivity : ComponentActivity(), OnRobotReadyListener, Robot.AsrListene
         mRobot.addAsrListener(this)
         mRobot.addOnConversationStatusChangedListener(this)
         mRobot.addOnCurrentPositionChangedListener(this)
-
         mRobot.addOnDetectionStateChangedListener(this)
         mRobot.addOnDetectionDataChangedListener(this)
         mRobot.addOnUserInteractionChangedListener(this)
@@ -81,7 +93,6 @@ class MainActivity : ComponentActivity(), OnRobotReadyListener, Robot.AsrListene
         mRobot.removeAsrListener(this)
         mRobot.removeOnConversationStatusChangedListener(this)
         mRobot.removeOnCurrentPositionChangedListener(this)
-
         Log.i(tag, "Set detection mode: OFF")
         mRobot.setDetectionModeOn(false, 2.0f) // Set detection mode off
         Log.i(tag, "Set track user: OFF")
@@ -200,6 +211,7 @@ class MainActivity : ComponentActivity(), OnRobotReadyListener, Robot.AsrListene
             android.Manifest.permission.READ_EXTERNAL_STORAGE,
             android.Manifest.permission.WRITE_EXTERNAL_STORAGE
         )
+        private const val PERMISSION_REQUEST_ACCESS_LOCATION = 100
 
         /**
          * Checks if the app has permission to write to device storage
@@ -215,6 +227,69 @@ class MainActivity : ComponentActivity(), OnRobotReadyListener, Robot.AsrListene
             if (permission != PackageManager.PERMISSION_GRANTED) {
                 // We don't have permission so prompt the user
                 ActivityCompat.requestPermissions(activity, PERMISSIONS_STORAGE, REQUEST_EXTERNAL_STORAGE)
+            }
+        }
+    }
+
+    private fun getCurrentLocation() {
+        if(checkPermission()) {
+            if(isLocationEnabled()) {
+                fusedLocationListener.lastLocation.addOnCompleteListener(this) {task ->
+                    val location: Location?=task.result
+                    if(location == null) {
+                        Toast.makeText(this, "Null Received", Toast.LENGTH_SHORT).show()
+                    }
+                    else {
+                        Toast.makeText(this, "Get success", Toast.LENGTH_SHORT).show()
+                        viewModel.longitude = location.longitude.toString()
+                        viewModel.latitude = location.latitude.toString()
+                    }
+                }
+            }
+            else {
+                Toast.makeText(this, "Turn on location", Toast.LENGTH_SHORT).show()
+                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(intent)
+            }
+        }
+        else {
+                requestPermission()
+        }
+    }
+
+    private fun checkPermission(): Boolean {
+        if(ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            return true
+        }
+        return false
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION,
+            android.Manifest.permission.ACCESS_COARSE_LOCATION), PERMISSION_REQUEST_ACCESS_LOCATION)
+    }
+
+
+    private fun isLocationEnabled(): Boolean {
+        val locationManager: LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if(requestCode == PERMISSION_REQUEST_ACCESS_LOCATION) {
+            if(grantResults.isNotEmpty() && grantResults[0]== PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(applicationContext, "Granted", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                Toast.makeText(applicationContext, "Denied", Toast.LENGTH_SHORT).show()
             }
         }
     }
