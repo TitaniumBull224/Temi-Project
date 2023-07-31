56a43f9f89700ea98159b9d8b33afec3d7d44bd9
VDT1805 <vdtoan1805@gmail.com>
2023-07-31 09:17:24 +0900

    New Devilvery

--- TemiFoodDelivery/app/src/main/java/com/ibsystem/temifooddelivery/MainActivity.kt
+++ TemiFoodDelivery/app/src/main/java/com/ibsystem/temifooddelivery/MainActivity.kt
@@ -1,11 +1,46 @@
 package com.ibsystem.temifooddelivery
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ibsystem.temifooddelivery.ui.theme.TemiFoodDeliveryTheme
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            TemiFoodDeliveryTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TemiFoodDeliveryTheme {
+        Greeting("Android")
     }
 }
